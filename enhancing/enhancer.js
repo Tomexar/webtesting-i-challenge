682a57b424b5560a9572b80483db00144eedff8a
Tomexar <tommyexar@gmail.com>
2019-06-17 17:30:27 -0600

    mvp complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,33 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  (item.enhancement == 20 ? item : item.enhancement++)
+  let newItem = Object.assign({}, item)
+  return { ...newItem };
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability < 5 ? item.durability = 0 : item.durability = item.durability - 5;
+  }
+
+  else if (item.enhancement == 15 || item.enhancement == 16) {
+    item.durability < 10 ? item.durability = 0 : item.durability = item.durability - 10;
+  }
+
+  else if (item.enhancement > 16){
+    item.durability < 10 ? item.durability = 0 : item.durability = item.durability -10;
+    item.enhancement--;
+  } 
+
+  let newItem= Object.assign({}, item)
+    return { ...newItem };
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100;
+  let newItem = Object.assign({}, item)
+  return { ...newItem };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,129 @@
 const enhancer = require('./enhancer.js');
-// test away!
+const { repair, succeed, fail } = require('./enhancer.js');
+
+
+describe('enhancer', () => {
+    describe('repair()', () => {
+        it('should restore durability to 100', () => {
+            const actual_repair = enhancer.repair({
+                name: 'item',
+                durability: 50,
+                enhancement: 20
+            });
+            const actual_repair2 = enhancer.repair({
+                name: 'item',
+                durability: 0,
+                enhancement: 10
+            })
+
+            expect(actual_repair.durability).toBe(100);
+            expect(actual_repair2.durability).toBe(100);
+            expect(repair({ durability: 89 }).durability).toBe(100);
+        });
+    });
+    describe('succeed()', () => {
+        it('should increase enhancement by 1, unless was already 20', () => {
+            const actual_succeed = enhancer.succeed({
+                name: 'item',
+                durability: 100,
+                enhancement: 20
+            });
+
+            const actual_succeed2 = enhancer.succeed({
+                name: 'item',
+                durability: 100,
+                enhancement: 10
+            });
+
+            expect(actual_succeed.enhancement).toBe(20);
+            expect(actual_succeed2.enhancement).toBe(11);
+            expect(succeed({ enhancement: 14 }).enhancement).toBe(15)
+            expect(succeed({ enhancement: 17 }).enhancement).toBe(18)
+        });
+    });
+
+    describe('fail()', ()=>{
+        it('should decrease the enhancement and durability', () =>{
+            const expected_fail1 = {
+                name:'item',
+                durability: 90,
+                enhancement: 15
+            }
+            const actual_fail1 = enhancer.fail({
+                name:'item',
+                durability:100,
+                enhancement:15
+            })
+            expect(actual_fail1).toStrictEqual(expected_fail1)
+
+            const expected_fail2 = {
+                name :'item',
+                durability: 0,
+                enhancement: 14
+            }
+            const actual_fail2 = enhancer.fail({
+                name: 'item',
+                durability: 4,
+                enhancement: 14
+            })
+            expect(actual_fail2).toStrictEqual(expected_fail2)
+
+            const expected_fail3 = {
+                name: 'item',
+                durability: 90,
+                enhancement: 15                
+            }
+            const actual_fail3 = enhancer.fail({
+                name: 'item',
+                durability: 100,
+                enhancement: 15
+            })
+            expect(actual_fail3).toEqual(expected_fail3);
+
+            const expected_fail4 = {
+                name: 'item',
+                durability: 0,
+                enhancement: 16
+            }
+            const actual_fail4 = enhancer.fail({
+                name: 'item',
+                durability: 8,
+                enhancement: 16
+            })
+            expect(actual_fail4).toEqual(expected_fail4);
+
+            const expected_fail5 = {
+                name: 'item',
+                durability: 50,
+                enhancement: 18 
+            }
+            const actual_fail5 = enhancer.fail({
+                name: 'item',
+                durability: 60,
+                enhancement: 19
+            })
+            expect(actual_fail5).toEqual(expected_fail5);
+
+            const expected_fail6 = {
+                name: 'item',
+                durability: 0,
+                enhancement: 19  
+            }
+            const actual_fail6 = enhancer.fail({
+                name: 'item',
+                durability: 6,
+                enhancement: 20
+            })
+            expect(actual_fail6).toEqual(expected_fail6);
+
+
+        })
+    })
+
+
+
+
+
+})
+
+
